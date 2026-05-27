8b1ddf1cf621c72abfb69a0e33d50eb0a4a15a09
SayNotes <sultangrowsgt@gmail.com>
2026-05-27 21:17:59 +0700

    fix: isu gambar

--- index.php
+++ index.php
@@ -80,7 +80,7 @@ $result = mysqli_query($conn, $query);
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                     </svg>
-                </button>
+                </a>
 
                 <div class="dropdown">
                     <div class="profile-group">
@@ -181,7 +181,7 @@ $result = mysqli_query($conn, $query);
 
                     <div class="sleek-card-index">
                         <div>
-                            <div class="preview-wrapper" style="background-image: url('<?php echo $image_url; ?>')">
+                            <div class="preview-wrapper" style="background-image: url('<?php echo $image_url; ?>'); aspect-ratio: 1 / 1; background-size: cover; background-position: center; border-radius: 8px;">
                                 <div class="floating-price">
                                     <img src="assets/img/coin.png" alt="coin" class="money-icon">
                                     Rp <?php echo number_format($row['harga'], 0, ',', '.'); ?>
