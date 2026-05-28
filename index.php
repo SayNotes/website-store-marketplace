c9ffe897a85dede94c4a75095fd2ee70ecaee7d5
SayNotes <sultangrowsgt@gmail.com>
2026-05-28 20:38:00 +0700

    style: tambah icon website

--- index.php
+++ index.php
@@ -46,6 +46,8 @@ $result = mysqli_query($conn, $query);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hamba Market</title>
+    
+    <link rel="icon" type="image/png" href="assets/img/coin.png">
     <link rel="stylesheet" href="assets/css/category-style.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
